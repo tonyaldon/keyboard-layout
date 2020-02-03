c3b2c099c2f3123e14c8488d0b7c02ebb0f52990
tony <aldon.tony@gmail.com>
2020-02-03 09:23:01 +0100

    Update LICENSE
    * LICENSE:  Update date.
    * install.sh:  Remove comments.

--- install.sh
+++ install.sh
@@ -6,25 +6,21 @@
 
 # [file] "evdev.xml"
 if [[ -e "backup/backup-evdev.xml" ]]; then
-		echo "[file: evdev.xml] You've already made a backup"
+    echo "[file: evdev.xml] You've already made a backup"
 else
-		mkdir backup
-		cp /usr/share/X11/xkb/rules/evdev.xml backup/backup-evdev.xml
-		echo "[file: evdev.xml] backup file has been created"
+    mkdir backup
+    cp /usr/share/X11/xkb/rules/evdev.xml backup/backup-evdev.xml
+    echo "[file: evdev.xml] backup file has been created"
 fi
-		
+
 rm -rf /usr/share/X11/xkb/rules/evdev.xml
 ln xkb/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
 
-# [file] "takbl" keyboard layout 
+# [file] "takbl" keyboard layout
 if [[ -e /usr/share/X11/xkb/symbols/takbl ]]; then
-		echo "[file: takbl] this keyboald layout already exist in directory:" 
+    echo "[file: takbl] this keyboald layout already exist in directory:"
     echo "/usr/share/X11/xkb/symbols/"
-		echo "you can't make hard link."
+    echo "you can't make hard link."
 else
-		ln xkb/takbl /usr/share/X11/xkb/symbols/takbl
+    ln xkb/takbl /usr/share/X11/xkb/symbols/takbl
 fi
-
-# [file] ~/.Xmodmaprc
-# this file is used to change the key Control 
-# see dotfiles configuration
